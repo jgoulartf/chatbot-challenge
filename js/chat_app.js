37078807134136564d0715062f163d9940287d0c
João Goulart <joao123goulart@gmail.com>
2023-02-28 12:51:08 -0300

    send message init

--- js/chat_app.js
+++ js/chat_app.js
@@ -1,71 +1,11 @@
-class ChatBot {
-    constructor() {
-        console.log(this.saudacaoInicial());
-    }
-
-    saudacaoInicial() {
-        return `Olá, sou o chatbot da TeleSoftware!
-        Estou aqui para te direcionar ao melhor atendimento
-        Digite (1) para Financeiro
-        Digite (2) para Orçamentos
-        Digite (3) para promoções
-        Digite (4) para Cancelamento`;
-    }
-
-    processaMensagem(msg) {
-
-        // Deixando todo o texto minúsculo
-        msg = msg.toLowerCase();
-        switch (msg) {
-            case "1" || "um" || "one":
-                return
-
-            case "2" || "dois" || "two":
-                return
-
-            case "3" || "três" || "tres" || "three":
-                return
-
-            case "4" || "quatro" || "four":
-                return
-
-            case "5" || "um" || "one":
-                return
-
-            default:
-                return "Não compreendo esse código, tente novamente...\n" + this.saudacaoInicial();
-        }
-
-
-    }
-}
-
-// Função principal que instacia o chatbot
-function bot() {
-    bot = new ChatBot();
-}
-
-/* 
-    Criar funcao para retornar um elemento HTML do tipo MINHA_MENSAGEM que segue o seguinte estilo: 
-    
-    <li class="clearfix">
-        <div class="message-data">
-            <span class="message-data-time">10:12 AM, Today</span>
-        </div>
-        <div class="message my-message">
-        TEXTO DA MENSAGEM AQUI
-        </div>
-    </li>
-
-*/
-
 // Escutando o input de nova mensagem
 document.addEventListener('DOMContentLoaded', () => {
     const newMessageInput = document.getElementById("new-message");
 
     const handleNewMessage = () => {
         const message = newMessageInput.value;
-        console.log('Nova mensagem: ', message);
+
+        sendMessage(message);
     };
     if (newMessageInput) {
         // Escutando o enter
@@ -82,13 +22,35 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 });
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-const createMyMessage = (msg) => {
+/* 
+    Criar funcao para retornar um elemento HTML do tipo MINHA_MENSAGEM que segue o seguinte estilo: 
+    
+    <li class="clearfix">
+        <div class="message-data">
+            <span class="message-data-time">10:12 AM, Today</span>
+        </div>
+        <div class="message my-message">
+        TEXTO DA MENSAGEM AQUI
+        </div>
+    </li>
+
+*/
+
+
+const sendMessage = (msg) => {
     time = horaFormatada();
     const chatHistorico = document.getElementById('chat-history');
+    console.log(msg)
+
+    
 
     chatHistorico.appendChild();
 }
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 /*
 
@@ -107,9 +69,23 @@ const createMyMessage = (msg) => {
 
 */
 
+
+const createOtherMessage = (msg) => {
+    time = horaFormatada();
+    const chatHistorico = document.getElementById('chat-history');
+
+    chatHistorico.appendChild();
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+
+
+// Funções auxiliares
 function horaFormatada() {
     const data = new Date();
     const horas = data.getHours().toString().padStart(2, '0'); // adiciona um zero à esquerda se a hora tiver apenas um dígito
     const minutos = data.getMinutes().toString().padStart(2, '0'); // adiciona um zero à esquerda se os minutos tiverem apenas um dígito
     return `${horas}:${minutos}`;
 }
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
